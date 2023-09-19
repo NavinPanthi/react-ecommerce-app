2275822fa4c37b3da9954ebf09ab33de392be3c4
Navin Panthi <panthinabin341@gmail.com>
2023-09-19 11:33:51 +0545

    adding redux persist

--- src/index.js
+++ src/index.js
@@ -6,12 +6,17 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+let persistor = persistStore(store);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemSlice from "./itemSlice";
 import cartSlice from "./cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  item: itemSlice.reducer,
+  cart: cartSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: {
-    item: itemSlice.reducer,
-    cart: cartSlice.reducer,
-  },
+  reducer: persistedReducer,
 });
 export default store;
